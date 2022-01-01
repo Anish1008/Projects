f8e8c71766ad0a2118e64d639e8c363547a055e4
Anish <anishrockagarwal1024@gmail.com>
2022-01-01 22:21:03 +0530

    initialized react redux

---
+++ Disney-plus-clone/disney-plus_clone/src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+function header() {
+    return (
+        <Container>
+            header
+        </Container>
+    )
+}
+
+export default header
+
+const Container= styled.div`
+
+
+
+`
