87c6b6e45be203d5f127683b5fd4201aea2ff3ea
Anish <anishrockagarwal1024@gmail.com>
2022-01-01 23:37:16 +0530

    Initialized header.js

---
+++ Disney-plus-clone/disney-plus-clone/src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+function header() {
+    return (
+        <Nav>
+            Header in nav
+        </Nav>
+    )
+}
+
+export default header
+
+const Nav = styled.nav`
+
+`
