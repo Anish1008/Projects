476bfb7786549ad684825633a05f4dd912559a47
Anish <anishrockagarwal1024@gmail.com>
2022-01-05 19:05:45 +0530

    Completing section

---
+++ Tesla Clone/tesla-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+// import styled from 'styled-components';
+import './App.css';
+import Header from './components/header';
+import Section from './components/section'
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Section/>
+    </div>
+  );
+}
+
+export default App;
