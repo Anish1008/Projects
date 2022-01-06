74a23b5a5ca8badbe80dc2b3a934f87861f807ae
Anish <anishrockagarwal1024@gmail.com>
2022-01-07 04:11:45 +0530

    Tesla clone completed and deployed

--- Tesla Clone/tesla-clone/src/App.js
+++ Tesla Clone/tesla-clone/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
-// import styled from 'styled-components';
 import './App.css';
 import Header from './components/header';
 import Section from './components/section'
@@ -11,25 +9,46 @@ function App() {
       <Header/>
       <Section title="Model S"
               description="Order online for Touchless Delivery"
-               image="model-s.jpg"/>
+               image="model-s.jpg"
+               leftbtntxt="Custom Order"
+               rightbtntxt="Existing Order"
+               />
       <Section title="Model Y" 
               description="Order online for Touchless Delivery"
-              image="model-y.jpg"/>
+              image="model-y.jpg"
+              leftbtntxt="Custom Order"
+               rightbtntxt="Existing Order"
+              />
       <Section title="Model 3" 
               description="Order online for Touchless Delivery"
-              image="model-3.jpg"/>
+              image="model-3.jpg"
+              leftbtntxt="Custom Order"
+               rightbtntxt="Existing Order"
+              />
       <Section title="Model X" 
               description="Order online for Touchless Delivery"
-              image="model-x.jpg"/>
+              image="model-x.jpg"
+              leftbtntxt="Custom Order"
+               rightbtntxt="Existing Order"
+              />
       <Section title="Solar Panels" 
               description="Lowest cose solar panels in America"
-              image="solar-panel.jpg"/>
+              image="solar-panel.jpg"
+              leftbtntxt="Order Now"
+               rightbtntxt="Learn More"
+              />
       <Section title="Solar Roofs" 
               description="Produce clean energy from your roof"
-              image="solar-roof.jpg"/>
+              image="solar-roof.jpg"
+              leftbtntxt="Order Now"
+               rightbtntxt="Learn More"
+              />
       <Section title="Accessories" 
               description=""
-              image="accessories.jpg"/>
+              image="accessories.jpg"
+              leftbtntxt="Shop Now"
+               rightbtntxt=""
+              />
     </div>
   );
 }
--- Tesla Clone/tesla-clone/src/components/section.js
+++ Tesla Clone/tesla-clone/src/components/section.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 import Fade from 'react-reveal/Fade'
-function section({title,description, image}) {
-    // console.log({image})
+import {selectCars} from "../features/car/carSlice"
+import {useSelector} from 'react-redux'
+function section({title,description, image, leftbtntxt, rightbtntxt}) {
+    // const cars = useSelector(selectCars)
     return (
         <>
         <Container bgImage={image}>
             <Fade bottom>
 
             <Heading>
-                <h1>{title}</h1>
+                <h1>{title} </h1>
                 <p>{description}</p>
             </Heading>
             </Fade>
             <Fade bottom>
 
             <Buttons>
-                <Left>Custom Order</Left>
-                <Right>Existing Inventory</Right>
+                <Left>{leftbtntxt}</Left>
+                {
+                    rightbtntxt && 
+                    <Right>{rightbtntxt}</Right>
+
+                }
             </Buttons>
             </Fade>
+            <Arrowdiv>
             <Downarrow src='/images/down-arrow.svg'/>
+
+            </Arrowdiv>
         </Container>
         </>
     )
@@ -35,14 +44,12 @@ background-size: cover;
 background-position:center;
 height:100vh;
 width:100vw;
-// border: 2px solid red;
 background-image: ${props => `url("/images/${props.bgImage}")`}
     `
     
     const Heading = styled.div`
     h1{
         font-size:40px;
-        // text-transform:uppercase;
         text-align: center;
         padding-top:20vh;
         opacity:1;
@@ -51,12 +58,6 @@ background-image: ${props => `url("/images/${props.bgImage}")`}
     }
     p{
         text-align: center;
-        a{
-            cursor: pointer;
-            &:hover{
-                text-decoration:underline;
-            }
-        }
     }
     `
     
@@ -64,15 +65,21 @@ background-image: ${props => `url("/images/${props.bgImage}")`}
     display:flex;
     justify-content:center;
     height:50px;
-    // width:100px;
     margin-top: 50vh;
+    @media(max-width:640px)
+    {
+        flex-direction:column;
+        margin-top:34vh;
+        height:130px;
+    }
+`
+const Arrowdiv = styled.div`
+height:60px;
+display:flex;
+justify-content:center;
+
 `
 const Downarrow = styled.img`
-    height:60px;
-    display:inline-block;
-    position:fixed;
-    bottom:0px;
-    left:47vw;
     animation-name:"bounce";
     animation-duration: 2.5s;
     animation-iteration-count: infinite;
@@ -90,31 +97,43 @@ color:white;
 background-color: rgba(23, 26, 32, 0.8);
 opacity:0.85;
 cursor:pointer;
+text-transform:uppercase;
+font-size:13px;
 display:flex;
 align-items:center;
 justify-content:center;
-font-weight:200;
-// border:2px solid black;
+font-weight:600;
 height:40px;
 width:256px;
 text-align:center;
 border-radius:100px;
 margin-right:26px;
+@media(max-width:640px){
+    justify-content:center;
+    width:100vw;
+    margin-bottom: 15px;
+}
 `
 const Right = styled.a`
 color:black;
 background-color: #ffffffb0;
+font-size:13px;
+text-transform:uppercase;
 opacity:0.85;
 cursor:pointer;
 display:flex;
 align-items:center;
 justify-content:center;
-font-weight:200;
-// border:2px solid black;
+font-weight:600;
 height:40px;
 width:256px;
 text-align:center;
 border-radius:100px;
+@media(max-width:640px){
+    justify-content:center;
+    width:100vw;
+    // margin: 10px 0;
+}
 
 
 `
