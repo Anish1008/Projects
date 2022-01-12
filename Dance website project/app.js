ea9fd307c7cac9851f85221084ecc8f61693b6b7
Anish <anishrockagarwal1024@gmail.com>
2022-01-12 22:00:58 +0530

    Started creating a dance website

---
+++ Dance website project/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path= require("path");
+const app = express();
+const port = 80;
+
+//Serving static files
+app.use("/static", express.static("static"));
+//Setting up pug
+app.set("view engine","pug");
+
+app.set("views",path.join(__dirname,"./components"));
+
+
+app.get("/", (req,res)=>{
+    res.status(200).render("index");
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Listening on localhost at port ${port}`)
+});
