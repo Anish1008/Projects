5aec690c6907e659f62399f4166fc7715f93e521
Anish <anishrockagarwal1024@gmail.com>
2022-04-22 02:01:29 +0530

    started with collection card

--- crypto-punk/src/App.js
+++ crypto-punk/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-
+import CollectionCard from './components/CollectionCard';
 function App() {
   return (
       <div className='app'>
       <Header />
-
+      <CollectionCard id="1" image="https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg" name="Banana Punk" traits={[{'value':10}]}/>
       </div>
   );
 }
